37907975aa88d03ea62f5f4807fb60c7b295fd41
Bajdzis <rafal@budzis.pl>
2021-06-29 17:54:37 +0200

    feat: generate graph file base on white spaces

--- src/classes/FileContent/FileContent.test.ts
+++ src/classes/FileContent/FileContent.test.ts
@@ -1,19 +1,41 @@
 import { PathInfo } from '../PathInfo/PathInfo';
 import { FileContent } from './FileContent';
 
+const content = `
+class Awesome {
+
+    doAwesomeStuff() {
+        // TODO: implement
+        // TODO: this
+        // TODO: function
+    }
+
+    doOtherAwesomeStuff() {
+        // TODO: implement!
+        return true;
+    }
+
+}
+`;
+
 describe('FileContent', () => {
 
     it('should get info about file and path' , () => {
         const path = new PathInfo('/home/awesomeClass.ts');
-        const content = `class Awesome {
-            doAwesomeStuff() {
-                // TODO: implement!
-            }
-        }`;
         const fileContent = new FileContent(path, content);
 
         expect(fileContent.getExtension()).toEqual('ts');
         expect(fileContent.getContent()).toEqual(content);
+    });
+
+    it('should generated correct nodes base on space number' , () => {
+        const path = new PathInfo('/home/awesomeClass.ts');
+        const fileContent = new FileContent(path, content);
+
+        const result = fileContent.getFileGraph();
+
+        expect(result.getContent()).toEqual(content);
+        expect(result).toMatchSnapshot();
     });
 });
 
--- src/classes/FileContent/FileContent.ts
+++ src/classes/FileContent/FileContent.ts
@@ -1,5 +1,10 @@
+import { FileContentNode } from '../FileContentNode/FileContentNode';
 import { PathInfo } from '../PathInfo/PathInfo';
 
+interface LineInfo {
+    line: string;
+    whiteSpaces:  number;
+}
 export class FileContent {
     private path: PathInfo;
     private content: string;
@@ -22,6 +27,95 @@ export class FileContent {
 
     getExtension(){
         return this.path.getExtension();
+    }
+
+    getFileGraph(): FileContentNode {
+        const file = new FileContentNode();
+        const lines = this.content.split('\n');
+
+        const countWhitespace = lines.map<LineInfo>((line, index) => {
+
+            const extendString = index === lines.length -1 ? line : `${line}\n`;
+
+            if (line.trim().length === 0) {
+                return {
+                    line: extendString,
+                    whiteSpaces: Math.max(
+                        FileContent.countNumberOfWhitespace(lines?.[index+1] || ''),
+                        FileContent.countNumberOfWhitespace(lines?.[index-1] || '')
+                    )
+                };
+            }
+
+            return {
+                line: extendString,
+                whiteSpaces: FileContent.countNumberOfWhitespace(line)
+            };
+        });
+
+        console.log({countWhitespace});
+
+        const children = this.nodeLine(countWhitespace);
+
+        return file.setChildren(children);
+    }
+
+    nodeLine(lines:LineInfo[]): FileContentNode[]{
+        if (lines.length === 0 ) {
+            return [];
+        }
+        if (lines.length === 1 ) {
+            return [new FileContentNode(lines[0].line)];
+        }
+        const baseWhitespace = lines[0].whiteSpaces;
+        let lastIndexWithBaseSpace = 0;
+        let omitElementsFromEnd = 0;
+        const result: FileContentNode[] = [];
+
+        for (let i = 1; i < lines.length; i++) {
+            const prevLine = lines[i-1];
+            const line = lines[i];
+
+
+            if(line.whiteSpaces === baseWhitespace) {
+                if(prevLine.whiteSpaces === line.whiteSpaces) {
+                    const node = new FileContentNode(prevLine.line);
+                    result.push(node);
+                    omitElementsFromEnd = 1;
+                } else {
+                    const start = lines[lastIndexWithBaseSpace];
+                    const node = new FileContentNode(start.line, line.line);
+                    const childrenNodes = this.nodeLine(lines.slice(lastIndexWithBaseSpace + 1, i));
+                    result.push(node.setChildren(childrenNodes));
+                    omitElementsFromEnd = 1;
+                    i++;
+                }
+                lastIndexWithBaseSpace = i;
+            } else {
+                omitElementsFromEnd++;
+            }
+        }
+
+        if(omitElementsFromEnd!==0) {
+            const omitLines = lines.slice(lines.length - omitElementsFromEnd, lines.length);
+            omitLines.forEach(line => {
+                result.push(new FileContentNode(line.line));
+            });
+        }
+
+        return result;
+    }
+
+    static countNumberOfWhitespace(s0: string){
+        for (let i = 0; i < s0.length; i++) {
+            const letter = s0[i];
+            if(letter === '\t' || letter === ' ') {
+                continue;
+            }
+            return i;
+        }
+
+        return 0;
     }
 
 }
